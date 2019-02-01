d1741cb4c403f557cf710d177422c5f1b00ce1c8
Simon Søndergaard <sisonder@microsoft.com>
2019-02-01 13:13:01 +0100

    release 1.5

--- src/main/java/com/microsoft/appcenter/appium/LabellingDriver.java
+++ src/main/java/com/microsoft/appcenter/appium/LabellingDriver.java
@@ -1,10 +1,10 @@
 package com.microsoft.appcenter.appium;
 
-/**
- * Label a point of time in a test. Will insert a screenshot and label {@code label} into test report when running
- * in test-cloud.
- * @param label text to insert into report.
- */
 public interface LabellingDriver {
+    /**
+     * Label a point of time in a test. Will insert a screenshot and label {@code label} into test report when running
+     * in test-cloud.
+     * @param label text to insert into report.
+     */
     void label(String label);
 }
